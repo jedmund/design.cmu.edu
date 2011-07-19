10fdc1e3873faa5becc44df9cb45fe198678e9af
Justin Edmund <justin@jedmund.com>
2011-07-19 08:33:51 -0400

    File structure, hooked up bottom bar interface element to the XML system.

--- school/index.php
+++ school/index.php
@@ -1,21 +1,25 @@
 <?php
 
-require_once('../lib/config.php');
-
-// Define the section variable
-$vars['section'] = 'school';
-
-$xml_str = file_get_contents(PUBLIC_PATH.DS.'assets'.DS.'content'.DS.'school.sidebar.xml');
-$sidenav = xmlstr_to_array($xml_str);
-$sidenav = $sidenav['item'];
-
-// Set current page
-$sidenav[0]['selected'] = 1;
-
-$vars['sidenav'] = $sidenav;
-
-
-$template = $twig->loadTemplate('school.what_is_design.html');
-echo $template->render($vars);
+  require_once('../lib/config.php');
+  
+  // Define the section variable
+  $vars['section'] = 'school';
+  
+  
+  $nav = generate_page_nav();
+  $nav[0]['selected'] = 1; // fix this to actually figure out the selected #.
+  
+  // Sets current page number, with 1-base.
+  // This controls the selected item in the sidenav as well as
+  // the bottom bar's contextual links.
+  $selected = 1;
+  
+  // Sets the variables for Twig.
+  $vars['sidenav']  = $nav;
+  $vars['page_no'] = $selected;
+  
+  // Render.
+  $template = $twig->loadTemplate('school.what_is_design.html');
+  echo $template->render($vars);
 
 ?>
